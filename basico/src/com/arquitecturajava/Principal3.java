bf6259d4f241bc378154277ba1d5e846d549fbd4
cecilioAlvarez <cmac@arquitecturajava.com>
2023-02-03 10:41:30 +0100

    sentencia if

---
+++ basico/src/com/arquitecturajava/Principal3.java
@@ -0,0 +1,26 @@
+package com.arquitecturajava;
+
+public class Principal3 {
+
+	public static void main(String[] args) {
+		
+		
+		int numero=4;
+		
+		if (numero>=5) {
+			System.out.println("aprobado");
+		}else {
+			System.out.println("suspenso");
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
