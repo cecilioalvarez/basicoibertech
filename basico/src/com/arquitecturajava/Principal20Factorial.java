c8db7f8859a86141cf370670b06db98dffe05c77
cecilioAlvarez <cmac@arquitecturajava.com>
2023-02-03 13:51:38 +0100

    recursividad

--- basico/src/com/arquitecturajava/Principal20Factorial.java
+++ basico/src/com/arquitecturajava/Principal20Factorial.java
@@ -3,20 +3,28 @@ package com.arquitecturajava;
 public class Principal20Factorial {
 
 	public static void main(String[] args) {
-		
 
-		System.out.println(factorial(5));
+		System.out.println(factorial2(5));
 	}
-	
-	public static int factorial (int numero) {
-		
-		int total=1;
-		
-		for (int i=1;i<=numero;i++) {
-			
-			total=total*i;
+
+	public static int factorial(int numero) {
+
+		int total = 1;
+
+		for (int i = 1; i <= numero; i++) {
+
+			total = total * i;
 		}
 		return total;
+	}
+
+	public static int factorial2(int numero) {
+
+		if (numero == 1)
+			return 1;
+		else
+			return numero * factorial2(numero - 1);
+
 	}
 
 }
