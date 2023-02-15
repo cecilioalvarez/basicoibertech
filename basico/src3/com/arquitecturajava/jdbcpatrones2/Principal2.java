cb07724f4c7a2b1fc92b456187e289e7f7253ee3
cecilioAlvarez <cmac@arquitecturajava.com>
2023-02-15 14:49:13 +0100

    reflection 2

--- basico/src3/com/arquitecturajava/jdbcpatrones2/Principal2.java
+++ basico/src3/com/arquitecturajava/jdbcpatrones2/Principal2.java
@@ -10,34 +10,75 @@ public class Principal2 {
 
 		PersonaActiveRecord p = new PersonaActiveRecord("pepe", "perez", 20);
 
-		Factura f= new Factura("1","ordenador",200);
-		imprimirPropiedades(p);
-		imprimirPropiedades(f);
+		Factura f = new Factura("1", "ordenador", 200);
+		cambiarPropiedades(p);
+		cambiarPropiedades(f);
 		System.out.println("****");
 
 	}
 
-	public static void imprimirPropiedades(Object o) {
+	public static void cambiarPropiedades(Object o) {
 
 		Class<?> clase = (Class<?>) o.getClass();
 
 		Method[] metodos = clase.getDeclaredMethods();
 
 		for (Method metodo : metodos) {
-
+			Object dato = null;
+			String metodoSet=null;
 			if (metodo.getName().startsWith("get")) {
-				//System.out.println(metodo.getName());
+				// System.out.println(metodo.getName());
 				try {
-					System.out.println(metodo.invoke(o, null));
+					metodoSet=metodo.getName().substring(3);
+					metodoSet="set"+metodoSet;
+					//System.out.println(metodoSet);
+				//	System.out.println(metodo.invoke(o));
+					dato = metodo.invoke(o);
+					System.out.println(dato);
+					
+					// buscame el metodo set que complementa a este get
+					Method m=buscarMetodo(metodos, metodoSet);
+					
+					//System.out.println("nombre metodo :"+m.getName());
+					//System.out.println(" tipo de dato"+dato.getClass());
+					
+					if (dato.getClass().getName().equals("java.lang.String")) {
+						
+						m.invoke(o, "holastring");
+						
+					}else if (dato.getClass().getName().equals("java.lang.Integer")) {
+						
+						m.invoke(o, 12345);
+					}else {
+						m.invoke(o, 12345.123);
+					}
+					
+					
+					dato = metodo.invoke(o);
+					System.out.println(dato);
+					
+					
 				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
+			
 		}
 
 		System.out.println("****");
 
+	}
+	
+	private static Method buscarMetodo(Method[] metodos, String nombre) {
+		
+		for (Method m : metodos) {
+			
+			if (m.getName().equals(nombre)) {
+				return m;
+			}
+		}
+		return null;
 	}
 
 }
