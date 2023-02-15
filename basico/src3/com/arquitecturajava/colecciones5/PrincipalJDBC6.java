8262d2103771e7871ff5c6091068af2ac2c6fd56
cecilioAlvarez <cmac@arquitecturajava.com>
2023-02-15 09:11:34 +0100

    insertar persona

---
+++ basico/src3/com/arquitecturajava/colecciones5/PrincipalJDBC6.java
@@ -0,0 +1,36 @@
+package com.arquitecturajava.colecciones5;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class PrincipalJDBC6 {
+
+	public static void main(String[] args) {
+
+		insertarPersona( new Persona ("paquito","sanchez",10));
+		
+	}
+
+	public static void insertarPersona(Persona persona) {
+
+		ConfiguradorDB c = new ConfiguradorDB();
+
+		try (Connection con = DriverManager.getConnection(c.getUrl(), c.getUser(), c.getPassword());
+				Statement s = con.createStatement();) {
+
+			String sql = "insert into Personas  (nombre,apellidos,edad) values ('"
+					+persona.getNombre()+ "','" + persona.getApellidos()+"',"+persona.getEdad()+")";
+			System.out.println(sql);
+			s.executeUpdate(sql);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
